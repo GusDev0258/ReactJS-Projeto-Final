df739fcdca332373c58a238185514507e8a60cc1
gusdev0258 <16326176760@edu.udesc.br>
2023-06-01 08:38:00 -0300

    feat: creating login

--- React-App/src/Components/Login/Login.jsx
+++ React-App/src/Components/Login/Login.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="register" element={<LoginRegister />} />
+        <Route path="recover-password" element={<LoginRecoverPassword />} />
+        <Route path="reset-password" element={<LoginResetPassword />} />
+      </Routes>
+    </div>
   )
 }
 
